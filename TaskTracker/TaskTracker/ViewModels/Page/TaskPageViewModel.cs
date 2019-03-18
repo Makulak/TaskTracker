819ea6c8f98fc5d635b7b4bcc74524daa9c4a39f
Jonasz Makulak <j.makulak@gmail.com>
2019-03-18 18:22:11 +0100

    Oh God, thaks

--- TaskTracker/TaskTracker/ViewModels/Page/TaskPageViewModel.cs
+++ TaskTracker/TaskTracker/ViewModels/Page/TaskPageViewModel.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TaskTracker.ViewModels.VM;
 
 namespace TaskTracker.ViewModels.Page
 {
     class TaskPageViewModel
     {
+        public TaskPageViewModel(TaskVM task)
+        {
+
+        }
     }
 }
